2f18856036e6f25e60deae6b4daa1aa3ca4f35c8
suryaatmajja <surya.uya1209@gmail.com>
2025-09-30 09:02:14 +0700

    update

--- src/pages/listView.jsx
+++ src/pages/listView.jsx
@@ -43,20 +43,22 @@ const ListView = () => {
           >
             <div className="flex justify-between items-center px-4">
               <div>
-                <h2 className="text-xs underline">Username</h2>
-                <h3 className="text-sm mt-1">{user.username}</h3>
+                <h2 className="text-xs underline md:text-sm">Username</h2>
+                <h3 className="text-sm mt-1 md:text-lg md:mt-0">
+                  {user.username}
+                </h3>
               </div>
 
               <div className="flex items-center gap-2">
                 <Link to={`/profil/${user.id}`}>
-                  <button className="hover:font-semibold bg-[rgba(255,209,70,1)] hover:bg-[rgba(255,195,20,1)] cursor-pointer px-2 py-1 rounded-lg text-sm">
+                  <button className="hover:font-semibold bg-[rgba(255,209,70,1)] hover:bg-[rgba(255,195,20,1)] cursor-pointer px-2 py-1 rounded-lg text-sm md:text-lg">
                     Edit
                   </button>
                 </Link>
 
                 <button
                   onClick={() => dispatch(removeUser(user.id))}
-                  className="hover:font-semibold bg-[rgba(227,61,49,1)] hover:bg-[rgba(204,54,44,1)] cursor-pointer px-2 py-1 rounded-lg text-sm"
+                  className="hover:font-semibold bg-[rgba(227,61,49,1)] hover:bg-[rgba(204,54,44,1)] cursor-pointer px-2 py-1 rounded-lg text-sm md:text-lg"
                 >
                   Delete
                 </button>
